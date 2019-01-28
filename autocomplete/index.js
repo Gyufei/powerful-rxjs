a3227acd5ac208cf03d3d61ae29fbcd8876656db
afei <guo812773673@163.com>
2019-01-28 22:12:19 +0800

    完成自动补全部分demo

--- autocomplete/index.js
+++ autocomplete/index.js
@@ -1,12 +1,16 @@
+/* global rxjs, fetch */
+const { fromEvent } = rxjs
+const { switchMap, debounceTime, map, filter } = rxjs.operators
+
+const inputDom = document.querySelector('#input-box')
+const list = document.querySelector('#sug-list')
+const input$ = fromEvent(inputDom, 'input')
+const select$ = fromEvent(list, 'click')
+
 function getSuggestionList (word) {
   const url = `https://zh.wikipedia.org/w/api.php?action=opensearch&format=json&limit=5&origin=*&search=${word}`
   return fetch(url, {mode: 'cors', method: 'GET'}).then(res => res.json())
 }
-
-const inputDom = document.querySelector('#input')
-const list = document.querySelector('#list')
-const input$ = fromEvent(inputDom, 'input')
-const select$ = fromEvent(list, 'click')
 
 function parseResultToList (arr = []) {
   list.innerHTML = ''
@@ -25,8 +29,8 @@ input$.pipe(
   debounceTime(400),
   switchMap(e => {
     return getSuggestionList(e.target.value)
-  }, (val, res) => res[1]  
-)
+  },
+  (val, res) => res[1])
 ).subscribe(
   arr => parseResultToList(arr)
 )
